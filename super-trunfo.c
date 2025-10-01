b189ccb5e27b1be559a7f752e783d1f377f165cf
melinn-dev <joaoavmmelo@gmail.com>
2025-10-01 16:44:05 -0300

    versao final super trunfo mestre

--- super-trunfo.c
+++ super-trunfo.c
@@ -12,7 +12,7 @@ int main()
        int turistico1, turistico2;
        float densidade1, densidade2;
        float pibpercapta1, pibpercapta2;
-
+       long double superpoder1, superpoder2;
        // mensagem inicial//
        printf("Bem vindo ao super trunfo!\n digite à seguir, os dados das cartas na ordem solicitada!\n");
        printf("----------------------carta 1----------------------\n");
@@ -31,9 +31,10 @@ int main()
        scanf("%lf", &pib1);
        printf("Quantidade de pontos turísticos\n");
        scanf("%d", &turistico1);
-       // calculo do pib e densidade a primeira carta
+       // calculo do pib e densidade a primeira carta e super-poder.
        densidade1 = (float)populacao1 / área1;
        pibpercapta1 = (double)(pib1 * 1000000000.0) / populacao1; // Convertendo bilhões para reais
+       superpoder1 = (float)populacao1 + área1 + ((float)pib1 * 1000000000.0) + turistico1 + pibpercapta1 + 1 / densidade1;
        // mensagem da segunda  carta//
        printf("----------------------carta 2----------------------\n");
        // cadastro da segunda carta//
@@ -54,12 +55,40 @@ int main()
        // calculo do pib e densidade da segunda carta
        densidade2 = (float)populacao2 / área2;
        pibpercapta2 = (double)(pib2 * 1000000000.0) / populacao2; // Convertendo bilhões para reais
+       superpoder2 = (float)populacao2 + área2 + ((float)pib2 * 1000000000.0) + turistico2 + pibpercapta2 + 1 / densidade2;
        // exibição das cartas//
        printf("----------------------carta 1----------------------\n");
-       printf("estado:%s \ncódigo: %s%s \nNome da cidade: %s\n população: %d \nárea %.f KM² \nPib: %lf Bilhões de reais \nNumero de pontos turísticos: %d \n densidade : %.2f\n Pib Per capta: %.2lf reais\n",
-              estado1, estado1, codigo1, nome1, populacao1, área1, pib1, turistico1, densidade1, pibpercapta1);
+       printf("estado:%s \ncódigo: %s%s \nNome da cidade: %s\n população: %d \nárea %.f KM² \nPib: %lf Bilhões de reais \nNumero de pontos turísticos: %d \n densidade : %.2f\n Pib Per capta: %.2lf reais\nsuperpoder de : %Lf\n",
+              estado1, estado1, codigo1, nome1, populacao1, área1, pib1, turistico1, densidade1, pibpercapta1, superpoder1);
        printf("----------------------carta 2----------------------\n");
-       printf("estado:%s \ncódigo: %s%s \nNome da cidade: %s\npopulacao: %d\n área: %.0f KM² \nPib: %.lf Bilhões de reais \nNumero de pontos turísticos: %d \n densidade : %.2f\n Pib Per capta: %.2lf reais\n",
-              estado2, estado2, codigo2, nome2, populacao2, área2, pib2, turistico2, densidade2, pibpercapta2);
-       return 0;   
+       printf("estado:%s \ncódigo: %s%s \nNome da cidade: %s\n população: %d \nárea %.f KM² \nPib: %lf Bilhões de reais \nNumero de pontos turísticos: %d \n densidade : %.2f\n Pib Per capta: %.2lf reais\nsuperpoder de : %Lf\n",
+              estado2, estado2, codigo2, nome2, populacao2, área2, pib2, turistico2, densidade2, pibpercapta2, superpoder2);
+
+       // comparaçao das cartas
+       /*int comparacaopopulacao1 =  populacao1 > populacao2;
+        int comparacaoarea1 = área1 > área2;
+        int comparacaopib1 = pib1 > pib2;
+        int comparacaopontos1 = turistico1 > turistico2;
+        int comparacaodensidade1 = densidade1 > densidade2;
+        int comparacaopibpercapita1 = pibpercapta1 > pibpercapta2;
+        int comparacaosuperpoder1 = superpoder1 > superpoder2;*/
+
+       printf("----------------------resultados----------------------\n");
+       printf(
+           "população: carta 1 venceu (%d)\n"
+           "área: carta 1 venceu (%d)\n"
+           "PIB: carta 1 venceu (%d)\n"
+           "pontos turísticos: carta 1 venceu (%d)\n"
+           "densidade populacional: carta 1 venceu (%d)\n"
+           "PIB per capita: carta 1 venceu (%d)\n"
+           "super poder: carta 1 venceu (%d)\n",
+           populacao1 > populacao2,
+           área1 > área2,
+           pib1 > pib2,
+           turistico1 > turistico2,
+           densidade1 > densidade2,
+           pibpercapta1 > pibpercapta2,
+           superpoder1 > superpoder2);
+
+       return 0;
 }
